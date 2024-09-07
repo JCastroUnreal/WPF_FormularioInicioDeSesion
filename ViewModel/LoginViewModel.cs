2085a9d4b317c3128c3d7124ef00602beb77259a
JCastroUnreal <JCastroUnreal@gmail.com>
2024-09-07 19:48:35 +0200

    "(app): add ViewModel and CustomControls"

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_FormularioInicioDeSesion.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        // Fields
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        // Properties
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get => _isViewVisible;
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        // Commands
+        public ICommand LoginCommand { get; }
+        public ICommand RecoveryPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+
+        // Constructor
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoveryPasswordCommand = new ViewModelCommand(p => ExecuteRecoveryPassCommand("", ""));
+        }
+
+        private void ExecuteRecoveryPassCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrEmpty(Username) || Username.Length < 3 || Password == null || Password.Length < 3)
+            {
+                validData = false;
+            } else
+            {
+                validData = true;
+            }
+
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
